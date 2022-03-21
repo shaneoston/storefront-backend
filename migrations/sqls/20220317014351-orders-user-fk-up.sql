32800b5284aa47f4cfe56fa64d02d356f5deccfd
Shane Oston Stowe <31624311+shaneoston@users.noreply.github.com>
2022-03-21 08:10:12 -0600

    adds before and after all to orders, adds constraints to orders.

--- migrations/sqls/20220317014351-orders-user-fk-up.sql
+++ migrations/sqls/20220317014351-orders-user-fk-up.sql
@@ -1,4 +1,6 @@
 ALTER TABLE orders
     ADD CONSTRAINT fk_orders_users
         FOREIGN KEY (user_id)
-            REFERENCES users(id);
+            REFERENCES users(id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE;
